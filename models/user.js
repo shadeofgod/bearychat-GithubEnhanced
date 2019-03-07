3a61b02bc1c611b942d2a745734f36f0c994f90a
zoubingwu <zoubingwu@gmail.com>
2019-03-07 18:01:53 +0800

    add user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+class UserModel {
+  getAccessToken(user_id, team_id, req) {
+    return new Promise((resolve, reject) => {
+      req.db.query(
+        `SELECT token FROM users WHERE user_id = ? AND team_id = ?`,
+        [user_id, team_id],
+        (error, results) => {
+        if (error) reject(error);
+        resolve(results);
+      });
+    });
+  }
+
+  saveToken(user_id, team_id, token, req) {
+    return new Promise((resolve, reject) => {
+      req.db.query(
+        `INSERT INTO users (user_id, team_id, token) VALUES (?, ?, ?)`,
+        [user_id, team_id, token],
+        (error, results) => {
+          if (error) reject(error);
+          resolve(results);
+        }
+      )
+    })
+  }
+}
+
+module.exports = new UserModel();
