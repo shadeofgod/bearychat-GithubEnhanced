368a762eccb2abce3ff05fd0a3b76d818e2c8d69
zoubingwu <zoubingwu@gmail.com>
2019-03-11 16:10:01 +0800

    refactor user model

--- models/user.js
+++ models/user.js
@@ -1,43 +1,35 @@
+function query(db, sql, inserts) {
+  return new Promise((resolve, reject) => {
+    db.query(sql, inserts, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
+  });
+};
+
 class UserModel {
-  getAccessToken(user_id, team_id, req) {
-    return new Promise((resolve, reject) => {
-      req.db.query(
-        `SELECT token FROM users
-        WHERE user_id = ? AND team_id = ?`,
-        [user_id, team_id],
-        (error, results) => {
-        if (error) reject(error);
-        resolve(results);
-      });
-    });
+  async getAccessToken(user_id, team_id, req) {
+    return await query(
+      req.db,
+      `SELECT token FROM users WHERE user_id = ? AND team_id = ?`,
+      [user_id, team_id]
+    );
   }
 
-  saveToken(user_id, team_id, token, req) {
-    return new Promise((resolve, reject) => {
-      req.db.query(
-        `INSERT INTO users (user_id, team_id, token)
-        VALUES (?, ?, ?)
-        ON DUPLICATE KEY UPDATE token = ?`,
-        [user_id, team_id, token, token],
-        (error, results) => {
-          if (error) reject(error);
-          resolve(results);
-        }
-      );
-    });
+  async saveToken(user_id, team_id, token, req) {
+    return await query(
+      req.db,
+      `INSERT INTO users (user_id, team_id, token) VALUES (?, ?, ?)`,
+      [user_id, team_id, token, token],
+    );
   }
 
-  removeToken(user_id, team_id, req) {
-    return new Promise((resolve, reject) => {
-      req.db.query(
-        `DELETE FROM users WHERE user_id = ? AND team_id = ?`,
-        [user_id, team_id],
-        (error, results) => {
-          if (error) reject(error);
-          resolve(results);
-        }
-      )
-    })
+  async removeToken(user_id, team_id, req) {
+    return await query(
+      req.db,
+      `DELETE FROM users WHERE user_id = ? AND team_id = ?`,
+      [user_id, team_id],
+    );
   }
 }
 
